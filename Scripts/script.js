81b2b844d341ef2ddb12fa44462db23670438885
Sierra A01341666 <SierraB.0048@gmail.com>
2022-11-23 13:00:55 -0800

    modifies onClick Button with pink bg color toggle script

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,10 +1,17 @@
 const clickMeButton = document.querySelector(".clickMe");
+// const body = document.querySelector(".body")
 
 function clickHandler ()
 {
-    alert("Clicked!")
-    // clickMeButton.removeEventListener("click", clickHandler);
+    clickMeButton.removeEventListener("click", clickHandler);
 };
 
-clickMeButton.addEventListener("click", clickHandler, {once:true});
+// clickMeButton.addEventListener("click", clickHandler, {once:true});
 
+function bgPink ()
+{
+    document.body.classList.toggle("pink")
+    // document.body.style.backgroundColor = "pink"
+}
+
+clickMeButton.addEventListener("click", bgPink);
