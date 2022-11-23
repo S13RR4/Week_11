a7c9a08c26c7ca93caf78fd06ddd7706a0002a43
Sierra A01341666 <SierraB.0048@gmail.com>
2022-11-23 14:38:23 -0800

    adds div buttons coloring on click

--- Scripts/script.js
+++ Scripts/script.js
@@ -48,5 +48,24 @@ function buttonsBgWhite(ev) {
         ev.target.classList.remove('bgGreenHover')
     }
 }
+function buttonsChangeTextColor(ev) {
+    let colour = ev.target.textContent
+    // if (ev.target.tagName != 'BUTTON') {
+    //     let colours = ev.target.textContent.split(' ')
+    //     for (item in colours){
+    //         if (item === '' || '\n') {
+    //             const index = colours.indexOf(item);
+    //             if (index > -1) { // only splice array when item is found
+    //             array.splice(index, 1); // 2nd parameter means remove one item only
+    //             }
+    //         }
+    //     }
+    //     console.log(colours)
+    //     let colour = colours[0]
+    // } 
+    // TYRING TO GET DIV TO CHANGE TOO, DIDN'T WORK, TOO BAD!
+    ev.target.classList.toggle(colour)
+}
 buttonContainer.addEventListener('mouseover', buttonsBgGreen)
 buttonContainer.addEventListener('mouseout', buttonsBgWhite)
+buttonContainer.addEventListener('click', buttonsChangeTextColor)
