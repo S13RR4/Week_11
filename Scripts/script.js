67409c1aa4721377aeec4c11d4d71261e1df5619
Sierra A01341666 <SierraB.0048@gmail.com>
2022-11-23 13:23:59 -0800

    modifies onClick button with text content toggle

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,17 +1,29 @@
-const clickMeButton = document.querySelector(".clickMe");
-// const body = document.querySelector(".body")
+const clickMeButton = document.querySelector('.clickMe');
+// const body = document.querySelector('.body')
 
 function clickHandler ()
 {
-    clickMeButton.removeEventListener("click", clickHandler);
+    clickMeButton.removeEventListener('click', clickHandler);
 };
 
-// clickMeButton.addEventListener("click", clickHandler, {once:true});
+// clickMeButton.addEventListener('click', clickHandler, {once:true});
 
 function bgPink ()
 {
-    document.body.classList.toggle("pink")
-    // document.body.style.backgroundColor = "pink"
+    document.body.classList.toggle('pink')
+    // document.body.style.backgroundColor = 'pink'
 }
 
-clickMeButton.addEventListener("click", bgPink);
+// clickMeButton.addEventListener('click', bgPink);
+
+function bgPinkToggle () {
+    if (document.body.classList.contains('pink')) {
+        clickMeButton.innerHTML = 'Click Me!'
+    }
+    else{
+        clickMeButton.innerHTML = 'Clicked'
+    }
+    document.body.classList.toggle('pink')
+}
+
+clickMeButton.addEventListener('click', bgPinkToggle);
